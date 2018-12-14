524711477806cc75b0db657dce2bae00964d2bbf
jpagnan0 <a.jpagnano@gmail.com>
2018-12-13 21:20:15 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,13 @@
+require "byebug"
 class StudentsController < ApplicationController
   def index
     @students = Student.all
+  end
+
+  def show
+    @student = Student.find(params[:id])
+    # @first_name = @student.first_name
+    # @last_name = @student.last_name
+    # binding.byebug
   end
 end
